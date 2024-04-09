b013a4ac9cbb5cb9ec912dd0525f37c60ac32c24
Jack Brouwer <jbrouwer2@rdw.nl>
2024-04-09 16:57:54 +0200

    Merge

---
+++ Determinator/Determinator.cs
@@ -0,0 +1,30 @@
+﻿namespace Determinator;
+using System;
+
+internal static class Determinator
+{
+    internal static short GetSimpleHashCode(string input, short maxValue)
+    {
+        var hashCode = Math.Abs(GetDeterministicHashCode(input));
+        return (short)(hashCode % maxValue);
+    }
+
+    private static int GetDeterministicHashCode(string input)
+    {
+        unchecked
+        {
+            int hash1 = (5381 << 16) + 5381;
+            int hash2 = hash1;
+
+            for (int i = 0; i < input.Length; i += 2)
+            {
+                hash1 = ((hash1 << 5) + hash1) ^ input[i];
+                if (i == input.Length - 1)
+                    break;
+                hash2 = ((hash2 << 5) + hash2) ^ input[i + 1];
+            }
+
+            return hash1 + (hash2 * 1566083941);
+        }
+    }
+}
---
+++ Determinator/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Discover your Hogwarts House!");
+Console.WriteLine("The Sorting Hat will place you into your rightful house.");
+Console.WriteLine("The Sorting Hat's decision is final...");
+Console.WriteLine("Start the sorting ceremony...");
+Console.WriteLine("What is your name?");
+
+var input = Console.ReadLine() ?? "";
+var code = Determinator.Determinator.GetSimpleHashCode(input, 4);
+
+Console.WriteLine($"You belong to.... {GetHouse(code)}!");
+
+static string GetHouse(short code) =>
+    code switch
+    {
+        1 => "Ravenclaw",
+        2 => "Hufflepuff",
+        3 => "Slytherin",
+        _ => "Gryffindor",
+    };
